744a72aba9f2dfb6d6e3dd4e52b3ae546cdb984f
sushmasingh2000 <sushmasingh16166@gmail.com>
2025-07-09 17:22:05 +0530

    gjdff

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-export const domain = 'http://192.168.18.101:9009';
-// export const domain = 'https://buybot.club';
+// export const domain = 'http://192.168.18.101:9009';
+export const domain = 'https://buybot.club';
 export const frontend = 'https://buybot.club';
 export const dollar = "$"
 
